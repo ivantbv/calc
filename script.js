44e32b59aba4e2cb246fd9a6b421bc95901af852
Ivan Georgiev <ivantbv@gmail.com>
2022-11-22 09:40:22 +0500

    fix bugs with pressing equal before entering all the numbers and operators. Add a rounding function that rounds numbers with long decimals.

--- script.js
+++ script.js
@@ -43,13 +43,20 @@ let displayValue = '';
 //when a number after the operator has been entered, clear the initial display
 
 function populateDisplay(e) {
+    e.preventDefault();
    if (e.target.textContent === '+' || e.target.textContent === '×' || e.target.textContent === '÷' || e.target.textContent === '−') {
     if (writingDisplay.textContent.includes('+') || writingDisplay.textContent.includes('×') || writingDisplay.textContent.includes('÷') || writingDisplay.textContent.includes('−')) {
       let operatorValue = getOperator(displayValue)
       let operands = splitDisplayValue(displayValue, operatorValue)
-  
-      writingDisplay.textContent = operate(operatorValue, +operands[0], +operands[1])
-      displayValue = operate(operatorValue, +operands[0], +operands[1])
+        if (!operands[1]) {
+            console.log('here', operands, operatorValue)
+
+            writingDisplay.textContent = rounding(operate(operatorValue, +operands[0], +operands[0]))
+            displayValue = rounding(operate(operatorValue, +operands[0], +operands[0]))
+        } else {
+      writingDisplay.textContent = rounding(operate(operatorValue, +operands[0], +operands[1]))
+      displayValue = rounding(operate(operatorValue, +operands[0], +operands[1]))
+        }
     
     }
   }
@@ -64,14 +71,22 @@ function populateDisplay(e) {
     let operatorValue = getOperator(displayValue)
     let operands = splitDisplayValue(displayValue, operatorValue)
 
-    writingDisplay.textContent = operate(operatorValue, +operands[0], +operands[1])
-    displayValue = operate(operatorValue, +operands[0], +operands[1])
-    return;
+    if (!operands[1]) {
+        writingDisplay.textContent = rounding(operate(operatorValue, +operands[0], +operands[0]))
+        displayValue = rounding(operate(operatorValue, +operands[0], +operands[0]))
+    } else {
+
+        writingDisplay.textContent = rounding(operate(operatorValue, +operands[0], +operands[1]))
+        displayValue = rounding(operate(operatorValue, +operands[0], +operands[1]))
+        console.log(operands, operatorValue, 'logging')
+        return;
+    }
   } 
   // else if (e.target.textContent == '+' || e.target.textContent == '×' || e.target.textContent == '÷' || e.target.textContent == '−') {
 
   // }
   else {
+
     if (!displayValue || displayValue.toString().slice().includes('undefined')) {
       displayValue = '';
     }
@@ -80,9 +95,6 @@ function populateDisplay(e) {
     writingDisplay.textContent += e.target.textContent
   }
 }
-//store the number before the operator
-//store the operator
-//store the number after the operator
 
 function splitDisplayValue(displValue, operator) {
   if (!displValue || !operator) {
@@ -92,15 +104,28 @@ function splitDisplayValue(displValue, operator) {
 }
 
 function getOperator(dispValue) {
-  if (!dispValue) {
-    return;
+    if (!dispValue) {
+      return;
+    }  else if (!dispValue.split('-')[0] && dispValue.includes('.')) {
+        let result = dispValue.split('-').join('').split('.')[1]
+        return result.match(/\D/)[0]
+     } else if (!dispValue.split('−')[0] || !dispValue.split('-')[0]) {
+        let result = dispValue.split('-').join('')
+        return result.match(/\D/)[0]
+    }
+    else if (dispValue.includes('.')) {
+        let result = dispValue.split('.').join('')
+        return result.match(/\D/)[0]
+    } else {
+    let arr = [] 
+    arr = dispValue.match(/\D/)
+    return arr[0]
+        }
   }
-  let arr = [] 
-  arr = dispValue.match(/\D/)
-  return arr[0]
+
+
+function rounding(x) {
+    return Math.round(x * 100000) / 100000
 }
 
-
-allButtons.forEach(btn => btn.addEventListener('click', populateDisplay))
-
-
+allButtons.forEach(btn => btn.addEventListener('mousedown', populateDisplay, true))
