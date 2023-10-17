4b483feee3db02fd829b767c8613d4265dbdf76a
Ivan Georgiev <ivantbv@gmail.com>
2023-10-17 17:10:44 +0500

    add styles and add basic functions for the calculator. Need to rework populateDisplay function

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const buttonsEl = document.querySelectorAll('.btn');
+const buttons = Array.from(buttonsEl);
+const writingDisplay = document.querySelector('.writing-display');
+const resultDisplay = document.querySelector('.result-display');
+
 const add = function(x,y) {
 	return x + y
 };
@@ -14,7 +19,64 @@ const sum = function([...toSum]) {
   return +totalSum;
 };
 
-const multiply = function([...toMultiply]) {
+const continousMultiplication = function([...toMultiply]) {
   return toMultiply.reduce((x,y) => x*y)
 };
 
+const multiply = function(x ,y) {
+  return x * y;
+}
+
+const divide = function(x, y) {
+  return x / y;
+}
+
+const operate = function(x, y, operator) {
+  if (operator === '*' || operator === '×') {
+    return multiply(x,y);
+  }
+  if (operator === '+') {
+    return add(x,y);
+  }
+  if (operator === '-') {
+    return subtract(x,y);
+  }
+  if (operator === '/') {
+    return divide(x,y);
+  }
+}
+
+const populateDisplay = function() {
+  let operator = '';
+  let firstNum = '';
+  let secondNum = '';
+  buttons.forEach(btn => btn.addEventListener('click', function(e){
+    let currentBtn = e.target.textContent;
+    if (!isNaN(currentBtn)) {
+      if (operator && firstNum && !secondNum) {
+        writingDisplay.textContent += currentBtn;
+        secondNum =  writingDisplay.textContent
+      }
+      if (!secondNum) {
+        writingDisplay.textContent += currentBtn;
+        firstNum = writingDisplay.textContent;
+      }
+    } else {
+      if (!firstNum && !secondNum) {
+        return;
+      }
+      if (currentBtn !== '=') {
+        operator = currentBtn;
+        writingDisplay.textContent = '';
+      }
+      if (currentBtn === '=' && firstNum && secondNum && operator) {
+        const result = operate(Number(firstNum), Number(secondNum), operator)
+        writingDisplay.textContent = result;
+        firstNum = result;
+        secondNum = '';
+      }
+    }
+  }))
+}
+
+populateDisplay();
